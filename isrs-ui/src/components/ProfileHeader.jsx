64e2e21a75899e2b5495a56b49da6f4f35877a85
MinhHwy <huyuwu55@gmail.com>
2026-01-21 07:38:22 +0700

    Add isrs-ui frontend

---
+++ isrs-ui/src/components/ProfileHeader.jsx
@@ -0,0 +1,12 @@
+const ProfileHeader = () => {
+    return (
+        <div className="profile-header">
+            <div className="header-content">
+                <h1>Student Dashboard</h1>
+                <p>Complete your profile to get better internship matches</p>
+            </div>
+        </div>
+    );
+};
+
+export default ProfileHeader;
